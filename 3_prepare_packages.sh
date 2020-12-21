345532df7922af560597b754f6abe0d1fb01ad56
eggydutch <59375032+eggydutch@users.noreply.github.com>
2020-12-21 21:45:54 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -11,8 +11,6 @@ fi
 [ ! -d luci-app-mentohust ] && git clone https://github.com/BoringCat/luci-app-mentohust
 [ ! -d mentohust ] && svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/mentohust
 rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-rm -rf luci-theme-edge/ && git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
-rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
 rm -rf v2ray/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
@@ -21,14 +19,14 @@ rm -rf trojan/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean
 cd ../../
 
 
-#git clone https://github.com/jerrykuku/lua-maxminddb.git
-#git clone https://github.com/jerrykuku/luci-app-vssr.git
-#cd luci-app-vssr/root/etc/
-#echo 'china_ssr.txt
-#config/black.list
-#config/white.list
-#dnsmasq.oversea/oversea_list.conf
-#dnsmasq.ssr/ad.conf
-#dnsmasq.ssr/gfw_base.conf
-#dnsmasq.ssr/gfw_list.conf' | xargs rm
-#cd ../../../
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+cd luci-app-vssr/root/etc/
+echo 'china_ssr.txt
+config/black.list
+config/white.list
+dnsmasq.oversea/oversea_list.conf
+dnsmasq.ssr/ad.conf
+dnsmasq.ssr/gfw_base.conf
+dnsmasq.ssr/gfw_list.conf' | xargs rm
+cd ../../../
